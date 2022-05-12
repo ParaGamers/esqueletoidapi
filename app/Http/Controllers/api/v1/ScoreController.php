f7fc89f806c2957b2247ce238641f9e9f4fe37d6
KevinCeferino <kevin.ceferino000@gmail.com>
2022-05-11 22:24:36 -0500

    score index

---
+++ app/Http/Controllers/api/v1/ScoreController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\api\v1\ScoreCollection;
+use App\Models\Score;
+use Illuminate\Http\Request;
+
+class ScoreController extends Controller
+{
+    
+    public function index()
+    {
+        return new ScoreCollection(Score::all());
+    }
+
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    
+    public function show($id)
+    {
+        //
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Resources/api/v1/ScoreResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\api\v1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ScoreResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => strtoupper($this->name),
+            'score' => (int)($this->score)
+        ];
+    }
+}
